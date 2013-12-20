7aa9a25ea4c4ec0561bd77695c20f44c5887ff59
Alexi Carey <ciegoproductions@gmail.com>
2013-12-20 16:34:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NadaDemo::Application.config.secret_token = '831a1ab4079b3927ffb80654dc36c78422cdd2ff437c75bcf7429f9730c3bfffaa9875e6cad0e0f4e486dabdded91728a99b6af71cc8bd7efa92f677afdae567'
